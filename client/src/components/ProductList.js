673b4cab33c13171774d83a975df2cd16fd3fe04
fahim-arif <fahim1.618555@gmail.com>
2021-09-13 06:02:39 +0600

    adding to master after basic admin dashboard done

--- client/src/components/ProductList.js
+++ client/src/components/ProductList.js
@@ -1,12 +1,105 @@
 import React from "react";
 import styles from "../components/styles/productList.module.css";
 import { Link } from "react-router-dom";
-const ProductList = () => {
+const ProductList = ({ product }) => {
+  console.log(product);
   return (
     <div className={styles.products_container}>
       <div className={styles.row}>
         <div className={styles.col}>
           <div className={styles.img_container}>
+            <Link to={`/shop/${product._id}`}>
+              <img src={product.image} alt='' className={styles.product_img} />
+            </Link>
+          </div>
+          <div className={styles.title}>
+            <Link to={`/shop/${product._id}`}>{product.name}</Link>
+          </div>
+          <div className={styles.price}>
+            <span className={styles.money_sign}>৳</span>
+            {product.price}
+          </div>
+          <div className={styles.sub_title}>Free Shipping</div>
+          <span className={styles.view_btn}>
+            <Link to={`/shop/${product._id}`}>VIEW DETAILS {">"}</Link>
+          </span>
+        </div>
+      </div>
+      {/* <div className={styles.col}>
+          <div className={styles.img_container}>
+            <Link to='#'>
+              <img
+                src='/images/shopPage/89-007-773-V08.jpg'
+                alt=''
+                className={styles.product_img}
+              />
+            </Link>
+          </div>
+          <div className={styles.title}>
+            <Link to='#'>
+              LG OLED77C1PUB 4K Smart OLED TV w/ AI THinQ (2021)
+            </Link>
+          </div>
+          <div className={styles.price}>
+            <span className={styles.money_sign}>৳</span>
+            3,296.99
+          </div>
+          <div className={styles.sub_title}>Free Shipping</div>
+          <span className={styles.view_btn}>
+            <Link to='#'>VIEW DETAILS {">"}</Link>
+          </span>
+        </div>
+        <div className={styles.col}>
+          <div className={styles.img_container}>
+            <Link to='#'>
+              <img
+                src='/images/shopPage/89-007-773-V08.jpg'
+                alt=''
+                className={styles.product_img}
+              />
+            </Link>
+          </div>
+          <div className={styles.title}>
+            <Link to='#'>
+              LG OLED77C1PUB 4K Smart OLED TV w/ AI THinQ (2021)
+            </Link>
+          </div>
+          <div className={styles.price}>
+            <span className={styles.money_sign}>৳</span>
+            3,296.99
+          </div>
+          <div className={styles.sub_title}>Free Shipping</div>
+          <span className={styles.view_btn}>
+            <Link to='#'>VIEW DETAILS {">"}</Link>
+          </span>
+        </div>
+        <div className={styles.col}>
+          <div className={styles.img_container}>
+            <Link to='#'>
+              <img
+                src='/images/shopPage/89-007-773-V08.jpg'
+                alt=''
+                className={styles.product_img}
+              />
+            </Link>
+          </div>
+          <div className={styles.title}>
+            <Link to='#'>
+              LG OLED77C1PUB 4K Smart OLED TV w/ AI THinQ (2021)
+            </Link>
+          </div>
+          <div className={styles.price}>
+            <span className={styles.money_sign}>৳</span>
+            3,296.99
+          </div>
+          <div className={styles.sub_title}>Free Shipping</div>
+          <span className={styles.view_btn}>
+            <Link to='#'>VIEW DETAILS {">"}</Link>
+          </span>
+        </div> */}
+      {/* <div className={styles.row}>
+        <div className={styles.col}>
+          <div className={styles.img_container}>
             <Link to='#'>
               <img
                 src='/images/shopPage/89-007-773-V08.jpg'
@@ -297,105 +390,7 @@ const ProductList = () => {
             <Link to='#'>VIEW DETAILS {">"}</Link>
           </span>
         </div>
-      </div>
-      <div className={styles.row}>
-        <div className={styles.col}>
-          <div className={styles.img_container}>
-            <Link to='#'>
-              <img
-                src='/images/shopPage/89-007-773-V08.jpg'
-                alt=''
-                className={styles.product_img}
-              />
-            </Link>
-          </div>
-          <div className={styles.title}>
-            <Link to='#'>
-              LG OLED77C1PUB 4K Smart OLED TV w/ AI THinQ (2021)
-            </Link>
-          </div>
-          <div className={styles.price}>
-            <span className={styles.money_sign}>৳</span>
-            3,296.99
-          </div>
-          <div className={styles.sub_title}>Free Shipping</div>
-          <span className={styles.view_btn}>
-            <Link to='#'>VIEW DETAILS {">"}</Link>
-          </span>
-        </div>
-        <div className={styles.col}>
-          <div className={styles.img_container}>
-            <Link to='#'>
-              <img
-                src='/images/shopPage/89-007-773-V08.jpg'
-                alt=''
-                className={styles.product_img}
-              />
-            </Link>
-          </div>
-          <div className={styles.title}>
-            <Link to='#'>
-              LG OLED77C1PUB 4K Smart OLED TV w/ AI THinQ (2021)
-            </Link>
-          </div>
-          <div className={styles.price}>
-            <span className={styles.money_sign}>৳</span>
-            3,296.99
-          </div>
-          <div className={styles.sub_title}>Free Shipping</div>
-          <span className={styles.view_btn}>
-            <Link to='#'>VIEW DETAILS {">"}</Link>
-          </span>
-        </div>
-        <div className={styles.col}>
-          <div className={styles.img_container}>
-            <Link to='#'>
-              <img
-                src='/images/shopPage/89-007-773-V08.jpg'
-                alt=''
-                className={styles.product_img}
-              />
-            </Link>
-          </div>
-          <div className={styles.title}>
-            <Link to='#'>
-              LG OLED77C1PUB 4K Smart OLED TV w/ AI THinQ (2021)
-            </Link>
-          </div>
-          <div className={styles.price}>
-            <span className={styles.money_sign}>৳</span>
-            3,296.99
-          </div>
-          <div className={styles.sub_title}>Free Shipping</div>
-          <span className={styles.view_btn}>
-            <Link to='#'>VIEW DETAILS {">"}</Link>
-          </span>
-        </div>
-        <div className={styles.col}>
-          <div className={styles.img_container}>
-            <Link to='#'>
-              <img
-                src='/images/shopPage/89-007-773-V08.jpg'
-                alt=''
-                className={styles.product_img}
-              />
-            </Link>
-          </div>
-          <div className={styles.title}>
-            <Link to='#'>
-              LG OLED77C1PUB 4K Smart OLED TV w/ AI THinQ (2021)
-            </Link>
-          </div>
-          <div className={styles.price}>
-            <span className={styles.money_sign}>৳</span>
-            3,296.99
-          </div>
-          <div className={styles.sub_title}>Free Shipping</div>
-          <span className={styles.view_btn}>
-            <Link to='#'>VIEW DETAILS {">"}</Link>
-          </span>
-        </div>
-      </div>
+      </div> */}
     </div>
   );
 };
